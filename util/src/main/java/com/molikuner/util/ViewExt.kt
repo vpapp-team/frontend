6041ade99efda923de7c85a7243446604852b0bb
molikuner <molikuner@gmail.com>
2018-08-25 22:50:26 +0200

    upload files and move into separate repo

--- util/src/main/java/com/molikuner/util/ViewExt.kt
+++
@@ -1,9 +0,0 @@
-package com.molikuner.util
-
-import android.support.design.widget.Snackbar
-import android.view.View
-
-
-fun View.showSnackBar(message: String, duration: Int) {
-    Snackbar.make(this, message, duration).show()
-}
